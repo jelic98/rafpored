d4786819e73ccc855792c5ea27d7b00b6a4356e7
Lazar Jelic <jelic.ecloga@gmail.com>
2018-05-27 20:13:22 +0200

    [FEAT] Caching API response

--- lib/controller/network/event_fetcher.dart
+++ lib/controller/network/event_fetcher.dart
@@ -1,31 +1,67 @@
+import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
+import 'package:http/http.dart' as Http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:rafpored/core/res.dart' as Res;
-import 'package:http/http.dart' as Http;
 import 'package:rafpored/core/utils.dart';
 import 'package:rafpored/model/event.dart';
 import 'package:rafpored/core/config.dart';
 import 'package:rafpored/controller/network/fetch_listener.dart';
-import 'dart:io';
 
 class EventFetcher {
 
-  static const cacheTimeout = 60 * 6; // minutes
+  static const int _cacheTimeout = 60 * 6; // minutes
+
+  static final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   static List<Event> allEvents;
 
   static fetchEvents(BuildContext context, FetchListener listener) {
-    _hasNetwork().then((ok) =>
-      (ok) ? _asyncFetch().then((events)
-          => _onSuccess(events, listener)) :
-          _onError(context, Res.Strings.alertNoNetwork, listener));
+    _checkCache(context, listener);
   }
 
-  static Future<List<Event>> _asyncFetch() async {
+  static _checkCache(BuildContext context, FetchListener listener) async {
+    String lastFetchTime, lastFetchData;
+
+    await _prefs.then((prefs) => lastFetchTime = prefs.getString("lastFetchTime"));
+    await _prefs.then((prefs) => lastFetchData = prefs.getString("lastFetchData"));
+
+    DateTime now = DateTime.now();
+
+    if(lastFetchTime != null && lastFetchTime.isNotEmpty &&
+        lastFetchData != null && lastFetchData.isNotEmpty) {
+      DateTime lastFetch = DateTime.parse(lastFetchTime);
+
+      if(now.difference(lastFetch).inMinutes < _cacheTimeout) {
+        _asyncFetch(lastFetchData).then((events) => _onSuccess(events, listener));
+        return;
+      }
+    }
+
+    _hasNetwork().then((ok) =>
+    (ok) ? _asyncFetch().then((events)
+    => _onSuccess(events, listener)) :
+    _onError(context, Res.Strings.alertNoNetwork, listener));
+
+    _prefs.then((prefs) => prefs.setString("lastFetchTime", now.toString()));
+  }
+
+  static Future<List<Event>> _asyncFetch([String data]) async {
     List<Event> events = List<Event>();
 
-    var response = JsonDecoder().convert((await Http.get(Config.getApiUrl("raspored/json.php"))).body);
+    var response;
+
+    if(data != null &&  data.isNotEmpty) {
+      response = data;
+    }else {
+      response = (await Http.get(Config.getApiUrl("raspored/json.php"))).body;
+    }
+
+    await _prefs.then((prefs) => prefs.setString("lastFetchData", response));
+
+    response = JsonDecoder().convert(response);
 
     var id = 0;
 
